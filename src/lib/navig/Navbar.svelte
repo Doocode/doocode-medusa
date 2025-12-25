865048db599d155e7e0b5df050086bc4428208fb
Aero15 <Aero15@users.noreply.github.com>
2025-12-25 23:43:04 +0100

    [Navbar] Use Shadcn Popover to display tools

--- src/lib/navig/Navbar.svelte
+++ src/lib/navig/Navbar.svelte
@@ -1,19 +1,14 @@
 <script>
-    import { fade } from "svelte/transition";
     import NavTools from "./tools/NavTools.svelte";
     import NavbarPages from "./pages/NavbarPages.svelte";
-
-    let displayTools = $state(false);
 </script>
 
 <header class="bg-primary text-primary-foreground text-center flex items-center justify-between px-1.5">
     <NavbarPages />
 
-    {#if !displayTools}
-        <h1 transition:fade={{ duration: 100 }}>
-            <a href="/" class="font-black text-2xl px-4 py-3 hover:underline">Doocode</a>
-        </h1>
-    {/if}
+    <h1>
+        <a href="/" class="font-black text-2xl px-4 py-3 hover:underline">Doocode</a>
+    </h1>
 
-    <NavTools bind:visible={displayTools} />
+    <NavTools />
 </header>
--- src/lib/navig/tools/NavTools.svelte
+++ src/lib/navig/tools/NavTools.svelte
@@ -1,40 +1,15 @@
 <script lang="ts">
     import Button from "$lib/components/ui/button/button.svelte";
-    import XIcon from "@lucide/svelte/icons/x";
     import CogIcon from "@lucide/svelte/icons/cog";
     import LanguageSwitcher from "./LanguageSwitcher.svelte";
     import DarkModeSwitcher from "./DarkModeSwitcher.svelte";
-    import { slide } from "svelte/transition";
-
-    interface Props {
-        visible?: boolean;
-    }
-
-    let {
-        visible = $bindable(false)
-    }: Props = $props();
+    import * as Popover from "$lib/components/ui/popover/index.js";
 </script>
 
-<div class="flex items-center gap-1 [&>div]:flex [&>div]:items-center [&>div]:gap-1 py-1.5">
-    {#if visible}
-        <div transition:slide={{ axis: 'x' }}>
-            <LanguageSwitcher />
-            <DarkModeSwitcher />
-
+<div class="flex items-center gap-1 [&_div]:flex [&_div]:items-center [&_div]:gap-1 py-1.5">
+    <Popover.Root>
+        <Popover.Trigger>
             <Button
-                onclick={() => (visible = false)}
-                variant="secondary"
-                size="icon"
-                class="h-11.25 w-11.25 ml-4"
-            >
-                <XIcon class="h-6! w-6!" />
-                <span class="sr-only">Close</span>
-            </Button>
-        </div>
-    {:else}
-        <div transition:slide={{ axis: 'x' }}>
-            <Button
-                onclick={() => (visible = !visible)}
                 variant="secondary"
                 size="icon"
                 class="h-11.25 w-11.25"
@@ -42,6 +17,13 @@
                 <CogIcon class="h-6! w-6!" />
                 <span class="sr-only">Settings</span>
             </Button>
-        </div>
-    {/if}
+        </Popover.Trigger>
+
+        <Popover.Content class="rounded-3xl">
+            <div class="flex justify-end gap-2">
+                <LanguageSwitcher />
+                <DarkModeSwitcher />
+            </div>
+        </Popover.Content>
+    </Popover.Root>
 </div>
