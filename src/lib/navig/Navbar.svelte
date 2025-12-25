51b3afdf6b26e909efeb8d835722330ad4cfee65
Aero15 <Aero15@users.noreply.github.com>
2025-12-25 23:31:36 +0100

    [Navbar] Build nav menu

--- src/lib/navig/Navbar.svelte
+++ src/lib/navig/Navbar.svelte
@@ -1,16 +1,13 @@
 <script>
-    import Button from "$lib/components/ui/button/button.svelte";
-    import MenuIcon from "@lucide/svelte/icons/menu";
     import { fade } from "svelte/transition";
     import NavTools from "./tools/NavTools.svelte";
+    import NavbarPages from "./pages/NavbarPages.svelte";
 
     let displayTools = $state(false);
 </script>
 
 <header class="bg-primary text-primary-foreground text-center flex items-center justify-between px-1.5">
-    <Button variant="secondary" size="icon" class="h-11.25 w-11.25">
-        <MenuIcon class="h-6! w-6!" />
-    </Button>
+    <NavbarPages />
 
     {#if !displayTools}
         <h1 transition:fade={{ duration: 100 }}>
---
+++ src/lib/navig/pages/NavbarPages.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import Button from "$lib/components/ui/button/button.svelte";
+    import MenuIcon from "@lucide/svelte/icons/menu";
+    import * as Popover from "$lib/components/ui/popover/index.js";
+    import PackageIcon from "@lucide/svelte/icons/package";
+    import BugIcon from "@lucide/svelte/icons/bug";
+    import { m } from "$lib/paraglide/messages";
+    import type { Component } from "svelte";
+    import { page } from '$app/state';
+
+    interface Item {
+        href: string;
+        icon: Component;
+        label: string;
+    }
+</script>
+
+<Popover.Root>
+    <Popover.Trigger>
+        <Button variant="secondary" size="icon" class="h-11.25 w-11.25">
+            <MenuIcon class="h-6! w-6!" />
+        </Button>
+    </Popover.Trigger>
+
+    <Popover.Content class="rounded-3xl">
+        <nav class="flex flex-col gap-1">
+            {#snippet item({
+                href, icon, label
+            }: Item) }
+                <a {href} class={{
+                    'flex items-center gap-2 px-3 py-2 rounded-lg duration-100 \
+                    hover:bg-secondary/50 hover:text-secondary-foreground \
+                    active:bg-secondary active:text-secondary-foreground': true,
+                    'bg-primary text-primary-foreground': page.url.pathname === href
+                }}>
+                    <svelte:component this={icon} class="h-5! w-5!"/>
+                    <span>{label}</span>
+                </a>
+            {/snippet}
+
+            {@render item({
+                href: '/projects', icon: PackageIcon,
+                label: m['projects.title']()
+            })}
+
+            {@render item({
+                href: '/demo/paraglide', icon: BugIcon,
+                label: 'Demo'
+            })}
+        </nav>
+    </Popover.Content>
+</Popover.Root>
