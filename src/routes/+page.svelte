02fc0ee35db4bfa33bc6ba41b5cc71525e90a5d4
Aero15 <Aero15@users.noreply.github.com>
2025-12-25 19:00:37 +0100

    Setup Shadcn UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import Button from '$lib/components/ui/button/button.svelte';
+
+    function test() {
+        console.log('test');
+    }
+</script>
+
+<svelte:head>
+    <title>Doocode</title>
+</svelte:head>
+
+<h1>Welcome to Doocode</h1>
+
+<Button variant="default" onclick={test}>
+    Test
+</Button>
+<Button variant="secondary" onclick={test}>
+    Test
+</Button>
