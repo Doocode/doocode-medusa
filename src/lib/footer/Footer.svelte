9d32cb7c01d690259abaf65bc44d0bd304d89dce
Aero15 <Aero15@users.noreply.github.com>
2025-12-27 19:27:06 +0100

    [Footer] Adjust margin/padding

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,6 +1,6 @@
 <footer>
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="py-6 border-t border-gray-200 dark:border-gray-700 text-center text-sm text-gray-500 dark:text-gray-400">
+    <div class="border-t border-gray-200 dark:border-gray-700 max-w-7xl mx-auto ">
+        <div class="py-6 px-4 text-center text-sm text-gray-500 dark:text-gray-400">
             Copyright &copy; {new Date().getFullYear()} Doocode. All rights reserved.
         </div>
     </div>
