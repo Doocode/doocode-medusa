c5f5e79ed96f0cdc354875784946bcc1cf18c6bc
Aero15 <Aero15@users.noreply.github.com>
2025-12-27 18:58:32 +0100

    [Projets] Add Dooscape and Capture projects

--- src/routes/projects/projects.ts
+++ src/routes/projects/projects.ts
@@ -1,4 +1,6 @@
 import { m } from "$lib/paraglide/messages";
+import CaptureLogo from './capture/logo.png';
+import DooscapeLogo from './dooscape/logo.png';
 import DoosearchLogo from './doosearch/logo.webp';
 import DoochronosLogo from './doochronos/logo.webp';
 
@@ -40,4 +42,25 @@ export const doochronos: Project = {
     bgAccent: { light: '#ffaed7', dark: '#db0386' },
 }
 
-export const projects: Project[] = [doosearch, doochronos];
+export const dooscape: Project = {
+    id: crypto.randomUUID(),
+    codename: 'dooscape',
+    name: 'Dooscape',
+    logo: DooscapeLogo,
+    catchyPhrase: m['projects.dooscape.catchy_phrase'](),
+    createdAt: new Date('2013-12-11'),
+    updatedAt: new Date('2016-01-20'),
+    bgAccent: { light: '#68f8bb', dark: '#20ae63' },
+};
+
+export const capture: Project = {
+    id: crypto.randomUUID(),
+    codename: 'capture',
+    name: 'Capture',
+    logo: CaptureLogo,
+    catchyPhrase: m['projects.capture.catchy_phrase'](),
+    createdAt: new Date('2015-03-31'),
+    bgAccent: { light: '#85ceff', dark: '#0078c8' },
+};
+
+export const projects: Project[] = [doosearch, doochronos, dooscape, capture];
