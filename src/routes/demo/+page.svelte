7aef7e3c09934d95e44bf73f1ca38e6a7b021d16
Aero15 <Aero15@users.noreply.github.com>
2025-12-27 17:36:22 +0100

    [Demo] Revamp demo pages

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
+	import Button from '$lib/components/ui/button/button.svelte';
+
+	import GamepadDirectional from '@lucide/svelte/icons/gamepad-directional';
 </script>
 
-<a href={resolve('/demo/buttons')}>buttons</a>
+<main class="max-w-7xl mx-auto px-4 py-8">
+	<h1 class="text-4xl font-black mb-4">Demo Pages</h1>
+	<p class="mb-8">This section contains various demo pages showcasing different UI components and features.</p>
+
+	<nav class="grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-2">
+		<Button href={resolve('/demo/buttons')} class="h-auto flex flex-col items-center gap-2 rounded-3xl active:scale-95 aspect-square">
+			<GamepadDirectional class="w-16! h-16!" />
+			<span>Buttons</span>
+		</Button>
+	</nav>
+</main>
--- src/routes/demo/buttons/+page.svelte
+++ src/routes/demo/buttons/+page.svelte
@@ -3,58 +3,69 @@
     import PinIcon from "@lucide/svelte/icons/pin";
 </script>
 
-<main class="max-w-7xl mx-auto p-4 flex flex-col gap-6 [&_td]:py-0.5">
-    <h1 class="text-3xl font-black">Buttons</h1>
+<main class="max-w-7xl mx-auto p-4 py-8 flex flex-col gap-6 [&_td]:py-0.5 [&_th]:text-start">
+    <strong class="-mb-6 text-primary hover:underline underline-offset-2 flex justify-start">
+        <a href="..">Demo</a>
+    </strong>
+    <h1 class="text-4xl font-black mb-4">Buttons</h1>
 
-    <h2 class="text-2xl font-bold">Variants</h2>
-    <table>
-        <tbody>
-            <tr>
-                <th class="w-37.5">Default</th>
-                <td><Button>Button</Button></td>
-            </tr>
-            <tr>
-                <th>Secondary</th>
-                <td><Button variant="secondary">Button</Button></td>
-            </tr>
-            <tr>
-                <th>Destructive</th>
-                <td><Button variant="destructive">Button</Button></td>
-            </tr>
-            <tr>
-                <th>Outline</th>
-                <td><Button variant="outline">Button</Button></td>
-            </tr>
-            <tr>
-                <th>Ghost</th>
-                <td><Button variant="ghost">Button</Button></td>
-            </tr>
-            <tr>
-                <th>Link</th>
-                <td><Button variant="link">Button</Button></td>
-            </tr>
-        </tbody>
-    </table>
+    <p class="mb-8">Demonstrates various button styles and sizes.</p>
 
-    <h2 class="text-2xl font-bold">Sizes</h2>
-    <table>
-        <tbody>
-            <tr>
-                <th>Icon</th>
-                <td>
-                    <Button size="icon">
-                        <PinIcon />
-                    </Button>
-                </td>
-            </tr>
-            <tr>
-                <th class="w-37.5">Small</th>
-                <td><Button size="sm">Button</Button></td>
-            </tr>
-            <tr>
-                <th>Large</th>
-                <td><Button size="lg">Button</Button></td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="grid gap-8 md:grid-cols-2">
+        <div>
+            <h2 class="text-2xl font-bold">Variants</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <th class="w-37.5">Default</th>
+                        <td><Button>Button</Button></td>
+                    </tr>
+                    <tr>
+                        <th>Secondary</th>
+                        <td><Button variant="secondary">Button</Button></td>
+                    </tr>
+                    <tr>
+                        <th>Destructive</th>
+                        <td><Button variant="destructive">Button</Button></td>
+                    </tr>
+                    <tr>
+                        <th>Outline</th>
+                        <td><Button variant="outline">Button</Button></td>
+                    </tr>
+                    <tr>
+                        <th>Ghost</th>
+                        <td><Button variant="ghost">Button</Button></td>
+                    </tr>
+                    <tr>
+                        <th>Link</th>
+                        <td><Button variant="link">Button</Button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div>
+            <h2 class="text-2xl font-bold">Sizes</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Icon</th>
+                        <td>
+                            <Button size="icon">
+                                <PinIcon />
+                            </Button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th class="w-37.5">Small</th>
+                        <td><Button size="sm">Button</Button></td>
+                    </tr>
+                    <tr>
+                        <th>Large</th>
+                        <td><Button size="lg">Button</Button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </main>
