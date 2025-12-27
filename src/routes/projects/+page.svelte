cb794b09633baef1cdec6ff22d3acbedcc8a67f6
Aero15 <Aero15@users.noreply.github.com>
2025-12-27 18:35:40 +0100

    [Projects] Add heading to the root page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,14 +2,17 @@
     import { m } from "$lib/paraglide/messages";
     import { projects } from "./projects";
     import ProjectCard from "./ProjectCard.svelte";
+    import Package from '@lucide/svelte/icons/package';
 </script>
 
 <svelte:head>
     <title>{m['projects.title']()} • Doocode</title>
 </svelte:head>
 
-<div class="m-auto max-w-7xl p-4">
-    <h1 class="text-3xl md:text-6xl font-bold">{m['projects.heading']()}</h1>
+<main class="max-w-7xl mx-auto px-4 py-8">
+    <Package class="w-20 h-20 mb-4 -mx-2 text-primary" />
+    <h1 class="text-4xl font-black mb-4">{m['projects.heading']()}</h1>
+    <p class="mb-8">{m['projects.description']()}</p>
 
     <div class="grid gap-4 grid-cols-[repeat(auto-fill,minmax(250px,1fr))] mt-8">
         {#each projects as { codename, name, logo, catchyPhrase, createdAt, updatedAt, bgAccent } }
@@ -20,4 +23,4 @@
             />
         {/each}
     </div>
-</div>
+</main>
