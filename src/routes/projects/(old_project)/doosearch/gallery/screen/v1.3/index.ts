fc7f2b77dd3dd806ffa475dd4790ce6a12930764
Aero15 <Aero15@users.noreply.github.com>
2025-12-30 18:40:41 +0100

    [Doosearch] Add more screenshots

--- src/routes/projects/(old_project)/doosearch/gallery/screen/v1.3/index.ts
+++ src/routes/projects/(old_project)/doosearch/gallery/screen/v1.3/index.ts
@@ -1,11 +1,18 @@
 import type { GalleryImageItem } from '$lib/gallery';
 
 import Img1 from './doosearch_1_3_screenshot_search.png';
+import ImgM1 from './doosearch_1_3_screenshot_search_mobile.png';
+import ImgM2 from './doosearch_1_3_screenshot_selectSearch_mobile.png';
+import ImgM3 from './doosearch_1_3_screenshot_searchEnginesWindow_mobile.png';
 import Img2 from './doosearch_1_3_screenshot_speeddial.png';
 import Img3 from './doosearch_1_3_screenshot_settings.png';
 import Img4 from './doosearch_1_3_screenshot_first_run.png';
 
 export const images: GalleryImageItem[] = [
+    {
+        src: ImgM1,
+        alt: "Search page on mobile",
+    },
     {
         src: Img1,
         alt: "Search page screenshot",
@@ -17,6 +24,14 @@ export const images: GalleryImageItem[] = [
     {
         src: Img3,
         alt: "Settings page",
+    },
+    {
+        src: ImgM3,
+        alt: "List of search engines on mobile",
+    },
+    {
+        src: ImgM2,
+        alt: "Selecting a search engine on mobile",
     },
     {
         src: Img4,
