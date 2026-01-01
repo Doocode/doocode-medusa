deddf3676fbbcef14eed98241393485dc1b6cf71
Aero15 <Aero15@users.noreply.github.com>
2026-01-01 20:22:11 +0100

    [Projects] Header: Resize icon

--- src/routes/projects/header/HeaderNavItem.svelte
+++ src/routes/projects/header/HeaderNavItem.svelte
@@ -19,8 +19,8 @@
 
 <Button
     class={{
-        "h-auto flex md:flex-col items-center justify-center text-center gap-1 md:gap-0.75 md:min-w-25 rounded-full md:rounded-2xl duration-150 \
-        hover:bg-foreground/60! hover:text-background hover:scale-105 active:scale-90": true,
+        "h-auto flex md:flex-col items-center justify-center text-center gap-1 md:gap-1.5 md:min-w-25 rounded-full md:rounded-xl md:aspect-square duration-100 \
+        hover:bg-foreground/60! hover:text-background active:scale-90": true,
         "text-background bg-foreground": selected,
         "cursor-pointer": href !== undefined,
     }}
@@ -30,7 +30,7 @@
 >
     {#if icon !== undefined}
         {@const Icon = icon }
-        <Icon class="w-5! h-5! md:w-16! md:h-16!" />
+        <Icon class="w-5! h-5! md:w-14! md:h-14!" />
     {/if}
     <span class="font-normal md:text-[13px]">{text}</span>
 </Button>
