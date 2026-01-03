7dd406adb54b407c94e468545ad7f5e8ac8aa8c7
Aero15 <Aero15@users.noreply.github.com>
2026-01-03 22:51:32 +0100

    [Doosearch] Gallery: Add 2 old logos

--- src/routes/projects/(old_project)/doosearch/gallery/logo/index.ts
+++ src/routes/projects/(old_project)/doosearch/gallery/logo/index.ts
@@ -1,30 +1,46 @@
 import type { GalleryImageItem } from '$lib/gallery';
 
-import currentLogo from './doosearch_logo_2026.webp';
-import oldLogoRound from './doosearch_logo_2016_round.png'
-import oldLogoFlat from './doosearch_logo_2016_flat.png'
+import logo_2026 from './doosearch_logo_2026.webp';
+import logo_2016Round from './doosearch_logo_2016_round.png'
+import logo_2016Flat from './doosearch_logo_2016_flat.png'
+import logo_2014_04 from './doosearch_logo_2014_04.png'
+import logo_2014_03 from './doosearch_logo_2014_03.png'
 
 export const logos: GalleryImageItem[] = [
     {
-        src: currentLogo,
+        src: logo_2026,
         alt: "Current logo - 2026",
         withTransparencyBg: true,
         description: "The current logo of Doosearch, introduced in 2026."
     },
     {
-        src: oldLogoRound,
+        src: logo_2016Round,
         alt: "Old logo (round) - 2016",
         withTransparencyBg: true,
     },
     {
-        src: oldLogoFlat,
+        src: logo_2016Flat,
         alt: "Old logo (flat) - 2016",
         withTransparencyBg: false,
+    },
+    {
+        src: logo_2014_04,
+        alt: "Old logo - April 2014",
+        withTransparencyBg: true,
+    },
+    {
+        src: logo_2014_03,
+        alt: "Old logo - March 2014",
+        withTransparencyBg: true,
     },
 ];
 
 export {
-    currentLogo,
-    oldLogoRound,
-    oldLogoFlat
+    // Current logo
+    logo_2026,
+    logo_2026 as currentLogo,
+
+    // Old versions
+    logo_2016Round,
+    logo_2016Flat
 }
