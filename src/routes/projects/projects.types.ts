f8d9501b62ae8f13e030263c8978ac36fe555bbd
Aero15 <Aero15@users.noreply.github.com>
2026-01-04 19:37:26 +0100

    [Projects] Create "Release" interface for changelog

--- src/routes/projects/projects.types.ts
+++ src/routes/projects/projects.types.ts
@@ -1,3 +1,5 @@
+import type { GalleryImageItem } from "$lib/gallery";
+
 export interface TwColor {
     light: string;
     dark: string;
@@ -38,4 +40,62 @@ export interface MainActionProps {
     href?: string;
     text: string;
     withAccent?: boolean;
+}
+
+export enum ReleaseType {
+    Alpha = 'Alpha',
+    Beta = 'Beta',
+    Stable = 'Stable',
+}
+
+export interface Release {
+    /**
+     * Release date
+     */
+    releaseDate: Date;
+
+    /**
+     * Version number (numeric format, e.g., 1.0, 2.5)
+     */
+    versionNumber: number;
+
+    /**
+     * Version as string (e.g., "v1.0.0", "v2.5.1")
+     */
+    versionString: string;
+
+    /**
+     * Release type (Alpha, Beta, or Stable)
+     */
+    type?: ReleaseType;
+
+    /**
+     * General release description
+     */
+    description?: string;
+
+    /**
+     * List of bug fixes
+     */
+    bugfixes?: string[];
+
+    /**
+     * List of new features
+     */
+    features?: string[];
+
+    /**
+     * List of improvements
+     */
+    improvements?: string[];
+
+    /**
+     * List of removed features
+     */
+    removedFeatures?: string[];
+
+    /**
+     * List of images
+     */
+    images?: GalleryImageItem[];
 }
