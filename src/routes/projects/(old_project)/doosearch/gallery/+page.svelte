c2876659d1bfcc969da9b43439446b22c38b0945
Aero15 <Aero15@users.noreply.github.com>
2026-01-04 18:37:01 +0100

    [Doosearch] Hide by default old screenshots

--- src/routes/projects/(old_project)/doosearch/gallery/+page.svelte
+++ src/routes/projects/(old_project)/doosearch/gallery/+page.svelte
@@ -2,12 +2,16 @@
     import { m } from "$lib/paraglide/messages";
     import Heading from '$lib/page/Heading.svelte';
 
-    import { DraftingCompass, PencilRuler, Fullscreen } from '@lucide/svelte/icons';
+    import { DraftingCompass, PencilRuler, Fullscreen, Eye, EyeOff } from '@lucide/svelte/icons';
 
     import { logos } from './logo';
     import { v1_2, v1_3 } from "./screen";
     import { Gallery } from "$lib/gallery";
     import { bts } from "./bts";
+    import { slide } from "svelte/transition";
+    import Button from "$lib/components/ui/button/button.svelte";
+
+    let displayOldVersion = $state(false);
 </script>
 
 <main class="max-w-7xl mx-auto pt-8 grid gap-8 [&_p]:text-balance">
@@ -20,9 +24,28 @@
         <p class="px-4 -mt-1.5 mb-2">Captures d'écran de la <strong class="text-primary">version 1.3</strong></p>
         <Gallery images={ v1_3 } size="large" />
 
-        <h3 class="text-primary font-bold px-4">Ancienne version</h3>
-        <p class="px-4 -mt-1.5 mb-2">Captures d'écran de la <strong class="text-primary">version 1.2</strong></p>
-        <Gallery images={ v1_2 } />
+        <div class="grid gap-2 px-4">
+            <h3 class="text-primary font-bold">Ancienne version</h3>
+            <p class="-mt-1.5 mb-2">Captures d'écran de la <strong class="text-primary">version 1.2</strong></p>
+
+            <nav>
+                <Button onclick={() => displayOldVersion = !displayOldVersion} class="mb-2">
+                    {#if displayOldVersion}
+                        <EyeOff class="w-5! h-5!" />
+                        Masquer les images
+                    {:else}
+                        <Eye class="w-5! h-5!" />
+                        Afficher les images
+                    {/if}
+                </Button>
+            </nav>
+        </div>
+        
+        {#if displayOldVersion}
+            <div transition:slide>
+                <Gallery images={ v1_2 } />
+            </div>
+        {/if}
     </div>
 
     <div class="grid gap-4">
