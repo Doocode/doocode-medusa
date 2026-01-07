3c8bdcf67a94ce99156b6e36af7148bddb127a44
Aero15 <Aero15@users.noreply.github.com>
2026-01-07 22:06:12 +0100

    [Doosearch] Changelog: Update layout

--- src/routes/projects/(old_project)/doosearch/history/[version]/+page.svelte
+++ src/routes/projects/(old_project)/doosearch/history/[version]/+page.svelte
@@ -70,7 +70,22 @@
     </div>
 </header>
 
-<main class="max-w-7xl mx-auto pt-8 px-4 pb-16 grid gap-8">
+<main class="max-w-7xl mx-auto px-4 pt-8 pb-16 grid gap-16">
+    <!-- Gallery -->
+    {#if release.images && release.images.length > 0}
+        <Restyle tag="section" class="grid gap-4 -mb-4"
+            tintLight="oklch(54.1% 0.281 293.009)"
+            tintDark="oklch(70.2% 0.183 293.541)"
+        >
+            <Heading level="h3" icon={Fullscreen}
+                title={ m['projects.screenshots']() }
+            />
+            <div class="-mx-4">
+                <Gallery images={release.images} size="large" />
+            </div>
+        </Restyle>
+    {/if}
+
     <!-- Description -->
     {#if release.description}
         <div class="prose dark:prose-invert max-w-none">
@@ -80,65 +95,47 @@
         </div>
     {/if}
 
-    <!-- Changes sections -->
-    <div class="grid gap-16">
-        <!-- Gallery -->
-        {#if release.images && release.images.length > 0}
-            <Restyle tag="section" class="mt-8 grid gap-4"
-                tintLight="oklch(54.1% 0.281 293.009)"
-                tintDark="oklch(70.2% 0.183 293.541)"
-            >
-                <Heading level="h3" icon={Fullscreen}
-                    title={ m['projects.screenshots']() }
-                />
-                <div class="-mx-4">
-                    <Gallery images={release.images} size="large" />
-                </div>
-            </Restyle>
-        {/if}
+    <!-- Features -->
+    {#if release.features && release.features.length > 0}
+        <Lister
+            icon={SquarePlus}
+            heading="New Features"
+            items={release.features}
+            tintLight="oklch(62.7% 0.194 149.214)"
+            tintDark="oklch(79.2% 0.209 151.711)"
+        />
+    {/if}
 
-        <!-- Features -->
-        {#if release.features && release.features.length > 0}
-            <Lister
-                icon={SquarePlus}
-                heading="New Features"
-                items={release.features}
-                tintLight="oklch(62.7% 0.194 149.214)"
-                tintDark="oklch(79.2% 0.209 151.711)"
-            />
-        {/if}
+    <!-- Improvements -->
+    {#if release.improvements && release.improvements.length > 0}
+        <Lister
+            icon={Sparkles}
+            heading="Improvements"
+            items={release.improvements}
+            tintLight="oklch(54.6% 0.245 262.881)"
+            tintDark="oklch(70.7% 0.165 254.624)"
+        />
+    {/if}
 
-        <!-- Improvements -->
-        {#if release.improvements && release.improvements.length > 0}
-            <Lister
-                icon={Sparkles}
-                heading="Improvements"
-                items={release.improvements}
-                tintLight="oklch(54.6% 0.245 262.881)"
-                tintDark="oklch(70.7% 0.165 254.624)"
-            />
-        {/if}
+    <!-- Bug Fixes -->
+    {#if release.bugfixes && release.bugfixes.length > 0}
+        <Lister
+            icon={Bug}
+            heading="Bug Fixes"
+            items={release.bugfixes}
+            tintLight="oklch(64.6% 0.222 41.116)"
+            tintDark="oklch(75% 0.183 55.934)"
+        />
+    {/if}
 
-        <!-- Bug Fixes -->
-        {#if release.bugfixes && release.bugfixes.length > 0}
-            <Lister
-                icon={Bug}
-                heading="Bug Fixes"
-                items={release.bugfixes}
-                tintLight="oklch(64.6% 0.222 41.116)"
-                tintDark="oklch(75% 0.183 55.934)"
-            />
-        {/if}
-
-        <!-- Removed Features -->
-        {#if release.removedFeatures && release.removedFeatures.length > 0}
-            <Lister
-                icon={Trash2}
-                heading="Removed Features"
-                items={release.removedFeatures}
-                tintLight="oklch(62.3% 0.234 25.406)"
-                tintDark="oklch(78.1% 0.174 25.406)"
-            />
-        {/if}
-    </div>
+    <!-- Removed Features -->
+    {#if release.removedFeatures && release.removedFeatures.length > 0}
+        <Lister
+            icon={Trash2}
+            heading="Removed Features"
+            items={release.removedFeatures}
+            tintLight="oklch(62.3% 0.234 25.406)"
+            tintDark="oklch(78.1% 0.174 25.406)"
+        />
+    {/if}
 </main>
