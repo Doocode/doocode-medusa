3571ebd1c16a507054665f819deb7031a1e5ce70
Aero15 <Aero15@users.noreply.github.com>
2026-01-11 18:49:04 +0100

    [Doosearch] Gallery: Add video example

--- src/routes/projects/(old_project)/doosearch/gallery/screen/v1.3/index.ts
+++ src/routes/projects/(old_project)/doosearch/gallery/screen/v1.3/index.ts
@@ -4,6 +4,7 @@ import Img_PC_01 from './doosearch_1_3_screenshot_search.png';
 import Img_PC_02 from './doosearch_1_3_screenshot_speeddial.png';
 import Img_PC_03 from './doosearch_1_3_screenshot_settings.png';
 import Img_PC_04 from './doosearch_1_3_screenshot_first_run.png';
+import Vid_PC_05 from './doosearch_1_3_screenshot_searchEnginesWindow_pc.mp4';
 
 import Img_M_01 from './doosearch_1_3_screenshot_search_mobile.png';
 import Img_M_02 from './doosearch_1_3_screenshot_selectSearch_mobile.png';
@@ -16,7 +17,7 @@ const img_m_01 = {
 };
 const img_m_02 = {
     src: Img_M_02,
-    alt: "Selecting a search engine on mobile",
+    alt: "Selection of a search engine on mobile",
 };
 const img_m_03 = {
     src: Img_M_03,
@@ -40,9 +41,14 @@ const img_pc_04 = {
     src: Img_PC_04,
     alt: "First run setup",
 };
+const vid_pc_05 = {
+    type: GalleryItemType.Video,
+    src: Vid_PC_05,
+    alt: "Selection of several search engines",
+};
 
-    img_m_01, img_pc_01, img_pc_02, img_pc_04,
 export const images: GalleryItemContent[] = [
+    img_m_01, img_pc_01, img_pc_02, img_pc_04, vid_pc_05,
     img_m_02, img_m_03, img_pc_03
 ];
 
@@ -50,6 +56,6 @@ export const mobile_images: GalleryItemContent[] = [
     img_m_01, img_m_02, img_m_03
 ];
 
-    img_pc_01, img_pc_02, img_pc_04, img_pc_03
 export const pc_images: GalleryItemContent[] = [
+    img_pc_01, img_pc_02, img_pc_04, img_pc_03, vid_pc_05,
 ];
