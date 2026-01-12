3db210420facebb4635208877feacb3d6b0fd65a
Aero15 <Aero15@users.noreply.github.com>
2026-01-12 13:36:25 +0100

    [Doosearch] Landing: Make new images clickable

--- src/routes/projects/(old_project)/doosearch/+page.svelte
+++ src/routes/projects/(old_project)/doosearch/+page.svelte
@@ -34,7 +34,7 @@
                     title="Qu'est-ce que Doosearch ?"
                 />
                 <Ul>
-                    <Li>Une sélection de plus de 100 <a href="#definition_01" class="underline">moteurs de recherche</a>*</Li>
+                    <Li>Une sélection de plus de 100 <a href="#definition_01" class="text-primary hover:underline">moteurs de recherche</a>*</Li>
                     <Li>Executer une requête sur plusieurs sites web en simultané</Li>
                     <Li>La personnalisation du fond d'écran et de couleur du thème</Li>
                     <Li>Utilisable depuis votre PC, tablette et smartphone</Li>
@@ -43,18 +43,22 @@
 
             <div class="
                 flex md:justify-center items-center md:mx-auto relative
-                row-start-0
-                md:row-start-1 lg:row-end-3 lg:col-start-2
-                [&_img]:rounded-md [&_img]:lg:rounded-b-2xl [&_img]:rotate-x-15 [&_img]:-rotate-y-15
+                row-start-0 md:row-start-1 lg:row-end-3 lg:col-start-2
+                [&_img]:rounded-md [&_img]:lg:rounded-b-2xl
+                [&_img]:rotate-x-15 [&_img]:-rotate-y-15
                 [&_img]:ring-4 md:[&_img]:ring-6 xl:[&_img]:ring-8
-                [&_img]:ring-background
+                [&_img]:ring-background [&_img]:hover:ring-primary
             ">
-                <img src={staticImages.Img_Search_PC}
-                    class="w-[80vw]"
-                    alt="Doosearch search page on a PC screen" />
-                <img src={staticImages.Img_Search_Mobile}
-                    class="w-[25vw] md:w-30 lg:w-40 xl:w-50 absolute right-0 md:-right-8 top-4"
-                    alt="Doosearch search page on a mobile screen" />
+                <a href={staticImages.Img_Search_PC} target="_blank" rel="noopener noreferrer">
+                    <img src={staticImages.Img_Search_PC}
+                        class="w-[80vw]"
+                        alt="Doosearch search page on a PC screen" />
+                </a>
+                <a href={staticImages.Img_Search_Mobile} target="_blank" rel="noopener noreferrer" class="absolute right-0 md:-right-8 top-4">
+                    <img src={staticImages.Img_Search_Mobile}
+                        class="w-[25vw] md:w-30 lg:w-40 xl:w-50"
+                        alt="Doosearch search page on a mobile screen" />
+                </a>
             </div>
 
             <div class="md:col-start-1 md:col-end-3 md:max-w-160 md:mx-auto lg:mx-0 lg:col-end-2 lg:mt-4 bg-primary/20 dark:bg-[#5e3416] p-4 md:p-8 rounded-2xl text-balance 2xl:-ml-8" id="definition_01">
