e0d4e735b7905bba6b29ccb93389b0fcebf35ec7
Aero15 <Aero15@users.noreply.github.com>
2026-01-14 22:39:03 +0100

    [Screenshots] Add prop to define gallery tile size

--- src/routes/projects/screenshots/Screenshots.svelte
+++ src/routes/projects/screenshots/Screenshots.svelte
@@ -6,9 +6,13 @@
 
     interface Props {
         releases: Release[];
+        size?: 'small' | 'medium' | 'large';
     }
 
-    let { releases = [] }: Props = $props();
+    let {
+        releases = [],
+        size = 'medium'
+    }: Props = $props();
 
     let sortedReleases = $derived([...releases].sort((a, b) => b.versionNumber - a.versionNumber));
     let currentVersionNumber = $state<number | undefined>(sortedReleases.length > 0 ? sortedReleases[0].versionNumber : undefined);
@@ -26,7 +30,7 @@
     <section class="bg-primary/30 dark:bg-primary/40 pt-4 pb-3 sm:rounded-4xl sm:overflow-clip">
         {#if selectedRelease && selectedRelease.images}
             {#key currentVersionNumber}
-                <Gallery images={ selectedRelease.images } size="large" />
+                <Gallery images={ selectedRelease.images } {size} />
             {/key}
         {/if}
     </section>
