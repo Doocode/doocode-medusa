75bcf0ec14db13dc8bf72774acf8df72072e0136
Aero15 <Aero15@users.noreply.github.com>
2026-01-14 22:41:18 +0100

    [Doosearch] Simplify layout to use Screenshots component

--- src/routes/projects/(old_project)/doosearch/gallery/+page.svelte
+++ src/routes/projects/(old_project)/doosearch/gallery/+page.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
+    import {
+        DraftingCompass, PencilRuler, Fullscreen, Construction
+    } from '@lucide/svelte/icons';
     import { m } from "$lib/paraglide/messages";
     import Heading from '$lib/page/Heading.svelte';
-
-    import { DraftingCompass, PencilRuler, Fullscreen, Eye, EyeOff, History, Construction } from '@lucide/svelte/icons';
-
     import { logos } from './logo';
-    import { v1_2, v1_3 } from "./screen";
     import { Gallery } from "$lib/gallery";
     import { bts, mockups } from "./bts";
-    import { slide } from "svelte/transition";
-    import Button from "$lib/components/ui/button/button.svelte";
-    import { resolve } from "$app/paths";
-
-    let displayOldVersion = $state(false);
+    import { Screenshots } from "$routes/projects/screenshots";
+    import { releases } from "../history/data";
 </script>
 
 <main class="container mx-auto pt-8 grid gap-16 [&_p]:text-balance">
@@ -21,37 +17,7 @@
             title={ m['projects.screenshots']() }
         />
 
-        <h3 class="text-primary font-bold px-4 mt-6">Version actuelle</h3>
-        <p class="px-4 -mt-1.5 mb-2">Captures d'écran de la <strong class="text-primary">version 1.3</strong></p>
-        <Gallery images={ v1_3 } size="large" />
-
-        <div class="grid gap-2 px-4 pt-8">
-            <h3 class="text-primary font-bold">Ancienne version</h3>
-            <p class="-mt-1.5 mb-2">Captures d'écran de la <strong class="text-primary">version 1.2</strong></p>
-
-            <nav class="mb-2 flex flex-wrap items-center gap-2">
-                <Button onclick={() => displayOldVersion = !displayOldVersion}>
-                    {#if displayOldVersion}
-                        <EyeOff class="w-5! h-5!" />
-                        Masquer les images
-                    {:else}
-                        <Eye class="w-5! h-5!" />
-                        Afficher les images
-                    {/if}
-                </Button>
-
-                <Button variant="secondary" href={resolve('/projects/doosearch/history')}>
-                    <History class="w-5! h-5!" />
-                    Voir l'historique des versions
-                </Button>
-            </nav>
-        </div>
-        
-        {#if displayOldVersion}
-            <div transition:slide>
-                <Gallery images={ v1_2 } />
-            </div>
-        {/if}
+        <Screenshots {releases} />
     </div>
 
     <div class="grid gap-4">
