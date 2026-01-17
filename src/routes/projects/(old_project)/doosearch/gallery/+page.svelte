6706d9461de6015e873f57ef11ea40c97230b614
Aero15 <Aero15@users.noreply.github.com>
2026-01-17 14:37:16 +0100

    [Doosearch] Gallery: Fix counter text color with bg

--- src/routes/projects/(old_project)/doosearch/gallery/+page.svelte
+++ src/routes/projects/(old_project)/doosearch/gallery/+page.svelte
@@ -28,8 +28,8 @@
     </div>
 
     <div class="grid gap-16 lg:grid-cols-2 xl:grid-cols-5 lg:gap-4 lg:-mt-12">
-        <div class="flex flex-col gap-4 lg:bg-blue-500/50 dark:lg:bg-chart-1/50 lg:rounded-4xl lg:overflow-clip lg:pt-6 lg:pb-4 xl:col-span-3">
-            <div class="grid gap-2 px-4 lg:px-8">
+        <article class="flex flex-col gap-4 sm:bg-blue-500/50 dark:sm:bg-chart-1/50 sm:rounded-3xl lg:rounded-4xl sm:overflow-clip sm:pt-4 lg:pt-6 lg:pb-4 xl:col-span-3">
+            <div id="bts" class="grid gap-2 px-4 lg:px-8 lg:flex lg:gap-8 lg:items-end lg:justify-between lg:[&>p]:max-w-100">
                 <Heading level="h2" icon={Construction}
                     title={ m['projects.bts']() }
                 />
@@ -37,10 +37,10 @@
             </div>
             <Gallery images={ bts } size="small"
                 class="mt-auto" classGallery="lg:px-8" classHeader="lg:px-8" />
-        </div>
+        </article>
 
-            <div class="grid gap-2 px-4 lg:px-8">
-        <div id="mockups" class="flex flex-col gap-4 lg:bg-chart-2/50 lg:rounded-4xl lg:overflow-clip lg:pt-6 lg:pb-4 xl:col-span-2">
+        <article id="mockups" class="flex flex-col gap-4 sm:bg-chart-2/50 sm:rounded-3xl lg:rounded-4xl sm:overflow-clip sm:pt-4 lg:pt-6 lg:pb-4 xl:col-span-2">
+            <div class="grid gap-2 px-4 lg:px-8 lg:flex lg:gap-8 lg:items-end lg:justify-between lg:[&>p]:max-w-80">
                 <Heading level="h2" icon={DraftingCompass}
                     title={ m['projects.mockups']() }
                 />
@@ -48,7 +48,15 @@
             </div>
             <Gallery images={ mockups } size="small"
                 class="mt-auto" classGallery="lg:px-8" classHeader="lg:px-8" />
-        </div>
+        </article>
+
+        <style>
+            @media (width >= 640px) {
+                article {
+                    --muted-foreground: var(--foreground);
+                }
+            }
+        </style>
     </div>
 
     <div id="logos" class="grid gap-4">
