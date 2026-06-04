2fb41b961f11aec835b2ed173b849edb8d2143dc
Aero15 <aero15@outlook.com>
2026-06-04 14:22:34 +0200

    [Demo] Create demo page to display LinkTile variants

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -6,36 +6,46 @@
 </script>
 
 <svelte:head>
-    <title>Demonstration • Doocode.xyz</title>
+	<title>Demonstration • Doocode.xyz</title>
 </svelte:head>
 
 <main class="container mx-auto px-4 py-8">
-	<FlaskConical class="w-20 h-20 mb-4 -mx-2 text-primary" />
-	<h1 class="text-4xl font-black mb-4">Demo Pages</h1>
-	<p class="mb-8">This section contains various demo pages showcasing different UI components and features.</p>
+	<FlaskConical class="-mx-2 mb-4 h-20 w-20 text-primary" />
+	<h1 class="mb-4 text-4xl font-black">Demo Pages</h1>
+	<p class="mb-8">
+		This section contains various demo pages showcasing different UI components and features.
+	</p>
 
 	<nav class="grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-2">
-		<Button href={resolve('/demo/heading')}
-			class="h-auto flex flex-col items-center gap-2 rounded-3xl active:scale-95 aspect-square">
-			<Heading6 class="w-16! h-16!" />
+		<Button
+			href={resolve('/demo/heading')}
+			class="flex aspect-square h-auto flex-col items-center gap-2 rounded-3xl active:scale-95"
+		>
+			<Heading6 class="h-16! w-16!" />
 			<span>Heading</span>
 		</Button>
 
-		<Button href={resolve('/demo/lists')}
-			class="h-auto flex flex-col items-center gap-2 rounded-3xl active:scale-95 aspect-square">
-			<List class="w-16! h-16!" />
+		<Button
+			href={resolve('/demo/lists')}
+			class="flex aspect-square h-auto flex-col items-center gap-2 rounded-3xl active:scale-95"
+		>
+			<List class="h-16! w-16!" />
 			<span>Lists</span>
 		</Button>
 
-		<Button href={resolve('/demo/buttons')}
-			class="h-auto flex flex-col items-center gap-2 rounded-3xl active:scale-95 aspect-square">
-			<GamepadDirectional class="w-16! h-16!" />
+		<Button
+			href={resolve('/demo/buttons')}
+			class="flex aspect-square h-auto flex-col items-center gap-2 rounded-3xl active:scale-95"
+		>
+			<GamepadDirectional class="h-16! w-16!" />
 			<span>Buttons</span>
 		</Button>
 
-		<Button href={resolve('/demo/projects')}
-			class="h-auto flex flex-col items-center gap-2 rounded-3xl active:scale-95 aspect-square">
-			<Package class="w-16! h-16!" />
+		<Button
+			href={resolve('/demo/projects')}
+			class="flex aspect-square h-auto flex-col items-center gap-2 rounded-3xl active:scale-95"
+		>
+			<Package class="h-16! w-16!" />
 			<span>Projects</span>
 		</Button>
 
--- src/routes/demo/projects/+page.svelte
+++ src/routes/demo/projects/+page.svelte
@@ -1,93 +1,107 @@
 <script lang="ts">
-    import { MainAction } from "../../projects/header";
-    import { Header } from "../../projects/header";
-    import { m } from "$lib/paraglide/messages";
-    import type { PageProps } from "./$types";
-    import ProjectCard from "../../projects/ProjectCard.svelte";
-    import BadgeQuestionMark from "@lucide/svelte/icons/badge-question-mark";
-    import ImagePlay from "@lucide/svelte/icons/image-play";
-    import Pointer from "@lucide/svelte/icons/pointer";
-    import QrCode from "@lucide/svelte/icons/qr-code";
-    import History from "@lucide/svelte/icons/history";
-    import { NavContener, NavItem } from "../../projects/header";
-    import { ReleaseTypeBadge } from "$routes/projects/changelog";
-    import { ReleaseType } from "$routes/projects/projects.types";
+	import { MainAction } from '$routes/projects/header';
+	import { Header } from '$routes/projects/header';
+	import { m } from '$lib/paraglide/messages';
+	import type { PageProps } from './$types';
+	import ProjectCard from '$routes/projects/ProjectCard.svelte';
+	import BadgeQuestionMark from '@lucide/svelte/icons/badge-question-mark';
+	import ImagePlay from '@lucide/svelte/icons/image-play';
+	import Pointer from '@lucide/svelte/icons/pointer';
+	import QrCode from '@lucide/svelte/icons/qr-code';
+	import History from '@lucide/svelte/icons/history';
+	import { NavContener, NavItem } from '$routes/projects/header';
+	import { ReleaseTypeBadge } from '$routes/projects/changelog';
+	import { ReleaseType, LinkType } from '$routes/projects/core';
+	import LinkTile from '$routes/projects/links/LinkTile.svelte';
 
-    let { data }: PageProps = $props();
+	let { data }: PageProps = $props();
 </script>
 
 <svelte:head>
-    <title>Projects demo • Doocode.xyz</title>
+	<title>Projects demo • Doocode.xyz</title>
 </svelte:head>
 
-<main class="container mx-auto p-4 py-8 flex flex-col gap-6">
-    <strong class="-mb-6 text-primary hover:underline underline-offset-2 flex justify-start">
-        <a href="/demo">Demo</a>
-    </strong>
-    <h1 class="text-4xl font-black mb-4">Projects</h1>
+<main class="container mx-auto flex flex-col gap-6 p-4 py-8">
+	<strong class="-mb-6 flex justify-start text-primary underline-offset-2 hover:underline">
+		<a href="/demo">Demo</a>
+	</strong>
+	<h1 class="mb-4 text-4xl font-black">Projects</h1>
 
-    <p class="mb-8">Preview of the projects components.</p>
+	<p class="mb-8">Preview of the projects components.</p>
 </main>
 
-<div class="container mx-auto px-4 mt-0 mb-12">
-    <h2 class="text-3xl font-black pb-4">Project card</h2>
+<div class="container mx-auto mt-0 mb-12 px-4">
+	<h2 class="pb-4 text-3xl font-black">Project card</h2>
 
-    <div class="grid gap-4 grid-cols-[repeat(auto-fill,minmax(250px,1fr))]">
-        <div class="grid gap-4">
-            <h3 class="text-xl font-bold">With accent color</h3>
-            <ProjectCard {...data.project_withAccent} />
-        </div>
-        <div class="grid gap-4">
-            <h3 class="text-xl font-bold">Without accent color</h3>
-            <ProjectCard {...data.project_withoutAccent} />
-        </div>
-    </div>
+	<div class="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4">
+		<div class="grid gap-4">
+			<h3 class="text-xl font-bold">With accent color</h3>
+			<ProjectCard {...data.project_withAccent} />
+		</div>
+		<div class="grid gap-4">
+			<h3 class="text-xl font-bold">Without accent color</h3>
+			<ProjectCard {...data.project_withoutAccent} />
+		</div>
+	</div>
 </div>
 
 <h2 class="container mx-auto px-4 pb-4 text-3xl font-black">Header</h2>
 {#snippet navBar()}
-    <NavContener>
-        <NavItem icon={ImagePlay} text="Click" onclick={() => alert('click')} />
-        <NavItem icon={Pointer} text="Link" href="#" />
-        <NavItem icon={QrCode} text="Hello" />
-        <NavItem icon={History} text="Hello" selected />
-        <NavItem icon={BadgeQuestionMark} text="Hello" />
-    </NavContener>
+	<NavContener>
+		<NavItem icon={ImagePlay} text="Click" onclick={() => alert('click')} />
+		<NavItem icon={Pointer} text="Link" href="#" />
+		<NavItem icon={QrCode} text="Hello" />
+		<NavItem icon={History} text="Hello" selected />
+		<NavItem icon={BadgeQuestionMark} text="Hello" />
+	</NavContener>
 {/snippet}
 <h3 class="container mx-auto px-4 py-2 text-xl font-bold">With accent color</h3>
 <Header project={data.project_withAccent}>
-    {#snippet mainAction()}
-        <MainAction variant="visit" href="#" withAccent
-            text={m['actions.visit']({ name: data.project_withAccent.name })}
-        />
-    {/snippet}
-    {@render navBar()}
+	{#snippet mainAction()}
+		<MainAction
+			variant="visit"
+			href="#"
+			withAccent
+			text={m['actions.visit']({ name: data.project_withAccent.name })}
+		/>
+	{/snippet}
+	{@render navBar()}
 </Header>
 
-<h3 class="container mx-auto px-4 py-2 mt-4 text-xl font-bold">Without accent color</h3>
+<h3 class="container mx-auto mt-4 px-4 py-2 text-xl font-bold">Without accent color</h3>
 <Header project={data.project_withoutAccent}>
-    {#snippet mainAction()}
-        <MainAction variant="visit" href="#" withAccent={false}
-            text={m['actions.visit']({ name: data.project_withoutAccent.name })}
-        />
-    {/snippet}
-    {@render navBar()}
+	{#snippet mainAction()}
+		<MainAction
+			variant="visit"
+			href="#"
+			withAccent={false}
+			text={m['actions.visit']({ name: data.project_withoutAccent.name })}
+		/>
+	{/snippet}
+	{@render navBar()}
 </Header>
 
-<div class="container mx-auto px-4 mt-12 mb-4">
-    <h2 class="text-3xl font-black pb-4">Changelog</h2>
+<div class="container mx-auto mt-12 mb-4 px-4">
+	<h2 class="pb-4 text-3xl font-black">Changelog</h2>
 
-    <div class="grid gap-4 grid-cols-[repeat(auto-fill,minmax(250px,1fr))]">
-        <div class="grid gap-4">
-            <h3 class="text-xl font-bold">Release type</h3>
+	<div class="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4">
+		<div class="grid gap-4">
+			<h3 class="text-xl font-bold">Release type</h3>
 
-            <div class="flex flex-wrap gap-2">
-                <ReleaseTypeBadge type={ReleaseType.Alpha} />
-                <ReleaseTypeBadge type={ReleaseType.Beta} />
-                <ReleaseTypeBadge type={ReleaseType.Stable} />
-            </div>
-        </div>
-        <div class="grid gap-4">
-        </div>
-    </div>
+			<div class="flex flex-wrap gap-2">
+				<ReleaseTypeBadge type={ReleaseType.Alpha} />
+				<ReleaseTypeBadge type={ReleaseType.Beta} />
+				<ReleaseTypeBadge type={ReleaseType.Stable} />
+			</div>
+		</div>
+	</div>
+
+	<div class="mt-12">
+		<h3 class="mb-6 text-2xl font-bold">Links (display="grid")</h3>
+		<div class="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+			{#each Object.values(LinkType) as type}
+				<LinkTile link={{ label: type, url: '#this-is-not-a-link', type }} display="grid" />
+			{/each}
+		</div>
+	</div>
 </div>
